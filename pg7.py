c198071c972eef525fc9602eda0cd9a5ce5198b6
Gurubalantu <jguru9289@gmail.com>
2025-08-18 10:50:54 +0530

    Delete pg7.py

--- pg7.py
+++
@@ -1,8 +0,0 @@
-print("circle program")
-print("------------")
-r=int(input("enter the r value"))
-print("result")
-perimeterofcircle=2*22/7*r
-print("perimeterofcircle:",perimeterofcircle)
-areaofcircle=22/7*r*r
-print("areaofcircle:",areaofcircle)
